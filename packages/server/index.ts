861838673387c4346c5bb58aa008c131e61f5d27
parthpurwar <parth.purwar.ece23@itbhu.ac.in>
2025-09-20 00:54:30 +0530

    Project setup

---
+++ packages/server/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import type { Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send(process.env.OPENAI_API_KEY);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});

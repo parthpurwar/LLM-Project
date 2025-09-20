d40e2cad6639ce4de8ea5fe6903827f9ede6683c
parthpurwar <parth.purwar.ece23@itbhu.ac.in>
2025-09-20 15:38:44 +0530

    include shadcun ui and setup prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,13 +1,19 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
-    const [message, setMessage] = useState("")
-    useEffect(() => {
-      fetch('/api/hello')
-      .then(res => res.json())
-      .then(data => setMessage(data.message))
-    }, []);
-  return <p className="font-bold p-4 text-3xl">{message}</p>
-  }
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    fetch('/api/hello')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+  return (
+    <>
+      <p className="font-bold p-4 text-3xl">{message}</p>
+      <Button>Button</Button>
+    </>
+  );
+}
 
-export default App
+export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,13 +1,13 @@
-import express from "express";
-import type { Request, Response } from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/api/hello', (req: Request, res: Response) => {
-  res.json({message: 'Hello World! Visit /api/hello for a message.'});
+  res.json({ message: 'Hello World! Visit /api/hello for a message.' });
 });
 
 app.listen(port, () => {
